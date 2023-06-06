32ed39800ea7bedfc3a08b4231b9e72aaca14e94
Kaito Suzuki <snails8d@gmail.com>
2023-06-07 08:15:33 +0900

    add: 共通利用を読み込むまで

--- apps/prj-a/src/App.tsx
+++ apps/prj-a/src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@org/ui-component';
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button onClick={() => console.log('click')}>test</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
